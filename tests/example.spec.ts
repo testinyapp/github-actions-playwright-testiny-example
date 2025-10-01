e9dbd7044e08d60b718801b4f0073fe11e85503b
Hanna Mueller <h.mueller@testiny.io>
2025-10-01 23:02:58 +0200

    add testkey annotations

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,26 +1,33 @@
 import { test, expect } from '@playwright/test';
 
-test('test that checks title (and fails) (has steps, annotations and tags)', { 
-  annotation: [
-    { type: 'type', description: 'smoke' },
-    { type: 'some_link', description: 'https://www.example.com' },
-  ],
-}, async ({ page }) => {
-  test.step("go to website", async () => {
-    await page.goto('https://www.testiny.io/');
+test.describe('Playwright Basic Example Tests', () => {
+  test('test that checks title (and fails) (has steps, annotations and tags)', { 
+    annotation: [
+      { type: 'testkey', description: 'atm-test-1' },
+      { type: 'type', description: 'smoke' },
+      { type: 'some_link', description: 'https://www.example.com' },
+    ],
+  }, async ({ page }) => {
+    await test.step("go to website", async () => {
+      await page.goto('https://www.testiny.io/');
+    });
+    await test.step("expect title", async () => {
+      await expect(page).toHaveTitle(/Not Testiny/);
+    })
   });
-  test.step("expect title", async () => {
-    await expect(page).toHaveTitle(/Not Testiny/);
-  })
-});
-
-test.skip('test something', async ({ page }) => {
-  // skipped test
-});
-
-test('test that is always', {
-  annotation: { type: 'some_link', description: 'https://www.example.com' },
-},
-  async ({ page }) => {
-  expect(true).toBe(true);
+  
+  test.skip('test something', {
+      annotation: { type: 'testkey', description: 'atm-test-2' },
+    }, async ({ page }) => {
+    // skipped test
+  });
+  
+  test.describe('Nested describe block', () => {
+    test('test that is always true', {
+      annotation: { type: 'testkey', description: 'atm-test-3' },
+    },
+      async ({ page }) => {
+      expect(true).toBe(true);
+    });
+  });
 });
